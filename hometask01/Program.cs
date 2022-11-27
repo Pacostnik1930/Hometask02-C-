fc452dbbf132dc7ba169262238cd2dcce3bcd2ac
Гойхман Петр <pgoyhman@mail.ru>
2022-11-27 11:42:41 +0300

    задача 01

---
+++ hometask01/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = int.Parse(Console.ReadLine());
+
+
+int result = number / 10 % 10;
+
+Console.WriteLine(result);
