976ac383291c3f898768dab32c961a6678a6c8c4
Гойхман Петр <pgoyhman@mail.ru>
2022-11-27 16:10:44 +0300

    задача 03

---
+++ hometask03/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1){
+    Console.WriteLine("понедельник,рабочий день");
+}
+if(number == 2){
+    Console.WriteLine("вторник,рабочий день");
+}
+if(number == 3){
+    Console.WriteLine("среда,рабочий день");
+}
+if(number == 4){
+    Console.WriteLine("четверг,рабочий день");
+}
+if(number == 5){
+    Console.WriteLine("пятница,рабочий день");
+}
+if(number == 6){
+    Console.WriteLine("Урааа!!Сегодня суббота,выходной");
+}
+if(number == 7){
+    Console.WriteLine("Урааа!!Сегодня воскресенье,выходной");
+}
