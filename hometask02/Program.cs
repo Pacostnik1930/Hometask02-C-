9c2d55c889877b4380461c0f4589ec0d2d1e5841
Гойхман Петр <pgoyhman@mail.ru>
2022-11-27 15:36:30 +0300

    задача 02

---
+++ hometask02/Program.cs
@@ -0,0 +1,10 @@
+﻿    
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if(numberText.Length > 2){
+    Console.WriteLine(numberText[2]);
+}
+else{
+    Console.WriteLine("третьей цифры нет");
+}
